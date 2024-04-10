36e5e7900056fada70d7b4126b84b7b79f35a9fc
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-04-10 16:00:59 +0700

    enable swagger in prod

--- Program.cs
+++ Program.cs
@@ -64,11 +64,11 @@ builder.Services.AddCors(option =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
